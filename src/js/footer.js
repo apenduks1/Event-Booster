d9d7ec4170d43cc74c4672eede416e67b89bf6c7
apenduks1 <126810268+apenduks1@users.noreply.github.com>
2025-01-27 19:41:44 +0200

    Опис змін

--- src/js/footer.js
+++ src/js/footer.js
@@ -8,11 +8,33 @@ footerBtns.forEach(btn => {
 
         });
         btn.classList.add("footer__btn--piced");
-        btn.disabled = true;
-        btn.parentElement.nextElementSibling.classList.remove("footer__item--unvisible")
-        btn.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.classList.add("footer__item--unvisible")
-
+    
         
+        if( +btn.textContent === 29){
+            
+        }else if(+btn.textContent === 1){
+
+        }
+        else if(+btn.textContent > 25){
+            btn.disabled = true;
+            btn.parentElement.nextElementSibling.classList.remove("footer__item--unvisible")
+            btn.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.classList.add("footer__item--unvisible")
+          
+        }
+        else if(+btn.textContent > 4){
+            btn.disabled = true;
+            btn.parentElement.nextElementSibling.classList.remove("footer__item--unvisible")
+            btn.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.classList.add("footer__item--unvisible")
+            btn.parentElement.previousElementSibling.classList.remove("footer__item--unvisible")
+            btn.parentElement.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.classList.add("footer__item--unvisible")
+        }
+        
+         else{
+            btn.disabled = true;
+            btn.parentElement.previousElementSibling.classList.remove("footer__item--unvisible")
+            btn.parentElement.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.classList.add("footer__item--unvisible")
+        }
+
     });
 });
 const footerBtnPiced = document.querySelector(".footer__btn--piced")
